40ffa19edafa7083f518758985b128542a84a852
Neel Mishra <neelmishra9125@gmail.com>
2025-03-17 19:08:16 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'FocusType - Word Mastery Practice',
   description: 'Master typing one word at a time with FocusType',
+  icons: {
+    icon: "/favicon.ico", 
+  }
 };
 
 export default function RootLayout({
